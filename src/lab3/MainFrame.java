c6d5136d37b52698706982af32b370dd78d59231
Nastya Leniuski <nastyamaseik@gmail.com>
2019-12-26 23:43:43 +0200

    Работа над классом MainFrame

--- src/lab3/MainFrame.java
+++ src/lab3/MainFrame.java
@@ -1,4 +1,49 @@
 package lab3;
 
-public class MainFrame {
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+
+public class MainFrame extends JFrame {
+
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+
+    // Массив коэффициентов многочлена
+    private Double[] coefficients;
+
+    // Объект диалогового окна для выбора файлов
+    // Компонент не создаѐтся изначально, т.к. может и не понадобиться
+    // пользователю если тот не собирается сохранять данные в файл
+    private JFileChooser fileChooser = null;
+
+    // Элементы меню вынесены в поля данных класса, так как ими необходимо
+    // манипулировать из разных мест
+    private JMenuItem saveToTextMenuItem;
+    private JMenuItem saveToGraphicsMenuItem;
+    private JMenuItem searchValueMenuItem;
+    private JMenuItem saveToCSVMenuItem;
+    private JMenuItem searchPrimeNumberItem;
+    private JMenuItem referenceMenuItem;
+
+    // Поля ввода для считывания значений переменных
+    private JTextField textFieldFrom;
+    private JTextField textFieldTo;
+    private JTextField textFieldStep;
+
+    private Box hBoxResult;
+
+    // Визуализатор ячеек таблицы
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+
+    // Модель данных с результатами вычислений
+    private GornerTableModel data;
+
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
 }
