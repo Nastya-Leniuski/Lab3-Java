3b9f0cbfaf3f323d5fc0631369c394edd6a474bb
Nastya Leniuski <nastyamaseik@gmail.com>
2019-12-26 23:46:48 +0200

    Работа с классом MainFrame над меню

--- src/lab3/MainFrame.java
+++ src/lab3/MainFrame.java
@@ -46,4 +46,57 @@ public class MainFrame extends JFrame {
     private GornerTableModel data;
 
     private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+
+    public MainFrame(Double[] coefficients) {
+        // Обязательный вызов конструктора предка
+        super("Табулирование многочлена на отрезке по схеме Горнера");
+        // Запомнить во внутреннем поле переданные коэффициенты
+        this.coefficients = coefficients;
+        // Установить размеры окна
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        // Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH) / 2, (kit.getScreenSize().height - HEIGHT) / 2);
+        // изменение формата числа
+        formatter.setMaximumFractionDigits(5);
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+        // Создать меню
+        JMenuBar menuBar = new JMenuBar();
+        // Установить меню в качестве главного меню приложения
+        setJMenuBar(menuBar);
+        // Добавить в меню пункт меню "Файл"
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        // Создать пункт меню "Таблица"
+        JMenu tableMenu = new JMenu("Таблица");
+        menuBar.add(tableMenu);
+        // Создать пункт меню "Справка"
+        JMenu referenceMenu = new JMenu("Справка");
+        menuBar.add(referenceMenu);
+        // Создать новое "действие" по сохранению в текстовый файл
+        Action saveToTextAction = new AbstractAction("Сохранить в текстовый файл") {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (fileChooser == null) {
+                    // Если экземпляр диалогового окна "Открыть файл" ещѐ не создан,
+                    // то создать его
+                    fileChooser = new JFileChooser();
+                    // и инициализировать текущей директорией
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                // Показать диалоговое окно
+                if (fileChooser.showSaveDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION) {
+                    // Если результат его показа успешный,
+                    // сохранить данные в текстовый файл
+                    saveToTextFile(fileChooser.getSelectedFile());
+                }
+            }
+        };
+        // Добавить соответствующий пункт подменю в меню "Файл"
+        saveToTextMenuItem = fileMenu.add(saveToTextAction);
+        // По умолчанию пункт меню является недоступным (данных ещѐ нет)
+        saveToTextMenuItem.setEnabled(false);
 }
